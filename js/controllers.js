a0fece33644fbee40ad9fdbe35f5fffecf9cd792
Greg Nicholas <greg.nicholas@gmail.com>
2013-05-01 15:09:31 -0400

    add first draft of clicking to add incident

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,22 @@
 app.controller('TrackerController', function($scope, dataService) {
-	var timeBuckets = dataService.getTimeBuckets();
+	var init = function() {
+		var timeBuckets = dataService.getTimeBuckets();
+		$scope.timeIncidentsPairs = _.map(timeBuckets, function(timeBucket) {
+			return {
+				timeBucket: timeBucket,
+				incidents: dataService.getIncidentsInBucket(timeBucket.id)
+			};
+		});
+		$scope.behaviorTypes = dataService.getBehaviorTypes();
+	};
 
-	$scope.timeIncidentsPairs = _.map(timeBuckets, function(timeBucket) {
-		return {
-			timeBucket: timeBucket,
-			incidents: dataService.getIncidentsInBucket(timeBucket.id)
-		};
-	});
+	$scope.addBehaviorIncident = function(timeBucketIndex) {
+		var behaviorTypeId = 1;
+		$scope.timeIncidentsPairs[timeBucketIndex].incidents.push({
+			cssClass: 'behavior-incident-' + behaviorTypeId,
+			behaviorType: dataService.getBehaviorTypes()[behaviorTypeId-1]
+		});
+	};
 
-	$scope.behaviorTypes = dataService.getBehaviorTypes();
-	console.log($scope);
+	init();
 });
