2a7997d3efee15d546a551c37b747294e57ca506
Greg Nicholas <greg.nicholas@gmail.com>
2013-05-23 12:20:35 -0400

    support tallies on scatterplot pane

--- js/controllers/scatter.js
+++ js/controllers/scatter.js
@@ -1,49 +1,56 @@
 app.controller('ScatterplotController', function($scope, dataService) {
 	var init = function() {
-		var incidentM2VM = function(model) {
-			return {
-				_model: model,
-				behaviorCode: model.get('behaviorType').get('code'),
-				cssClass: 'scatterplot-point-' + model.get('behaviorType').get('id')
-			};
-		};
+		var fixedDayModel = dataService.getDayRecordCollection().models[0];
 
-		var allIncidents = dataService.getBehaviorIncidents();
+		var behaviorTypeCollection = dataService.getBehaviorTypeCollection();
+		var behaviorIncidentCollection = dataService.getBehaviorIncidents();
+		var timeRecordCollection = dataService.getTimeRecordCollection();
+
+		function getIncidents(behaviorTypeId, timeRecordId, dayRecord) {
+			var behaviorTypeModel = behaviorTypeCollection.get(behaviorTypeId);
+			var timeRecordModel = timeRecordCollection.get(timeRecordId);
+			if (!behaviorTypeModel || !timeRecordModel) {
+				console.error('invalid ids. cannot get incidents')
+			}		
+			return behaviorIncidentCollection.where({time: timeRecordModel, day: fixedDayModel, behaviorType: behaviorTypeModel});
+		}
 
 		function Cell(dayRecordModel, timeBucketModel) {
-			var incidentModels = allIncidents.where({
-				day: dayRecordModel,
-				time: timeBucketModel
-			});
-			this.behaviorTypeModels = _.map(incidentModels, function(model) {
-				return model.get('behaviorType');
-			});
-		}
-
-		function BehaviorTypeDisplay(behaviorTypeModel, show) {
-			if (_.isUndefined(show)) {
-				show = true;
+			this.markers = [];
+			// leave the cell blank if we're not trying to render the fixed day
+			if (dayRecordModel) {
+				behaviorTypeCollection.each(function(behaviorType) {
+					var incidents = getIncidents(behaviorType.id, timeBucketModel.id, dayRecordModel)
+					if (incidents.length > 0) {
+						this.markers.push({
+							behaviorTypeId: behaviorType.id,
+							code: behaviorType.get('code'),
+							count: incidents.length
+						});
+					}
+				}, this);
 			}
-			this.show = show;
-			this.model = behaviorTypeModel;
 		}
 
-		$scope.behaviorTypeCollection = dataService.getBehaviorTypeCollection();
-
-		$scope.showBehaviorTypeStatus = {};
-		$scope.behaviorTypeCollection.each(function(typeModel) {
-			$scope.showBehaviorTypeStatus[typeModel.id] = true;
+		var options = behaviorTypeCollection.map(function(behaviorType) {
+			return {
+				id: behaviorType.id,
+				label: behaviorType.get('name')
+			};
 		});
+
+		
+		$scope.behaviorTypes = [{id: '0', label: 'All Behaviors'}].concat(options);
+		$scope.selectedBehaviorType = $scope.behaviorTypes[0];
+		$scope.tallyMode = false;
 
 		$scope.dayLabels = _.range(4,32);
 
 		$scope.grid = []; // 2d array: [time][day]
 
-		var fixedDayModel = dataService.getDayRecordCollection().models[0];
-
-		dataService.getTimeRecordCollection().each(function(timeRecord) {
+		timeRecordCollection.each(function(timeRecord) {
 			var row = {
-				time_string: timeRecord.get('iso_string'),
+				isoTimeString: timeRecord.get('iso_string'),
 				cells: []
 			};
 
