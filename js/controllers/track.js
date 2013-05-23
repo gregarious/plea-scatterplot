a521352c3be9a2112be216707a401cb0f9862fe1
Greg Nicholas <greg.nicholas@gmail.com>
2013-05-23 09:40:38 -0400

    switch to active bucket and button press functionality

--- js/controllers/track.js
+++ js/controllers/track.js
@@ -7,10 +7,14 @@ Array.prototype.remove = function(from, to) {
 app.controller('TrackerController', function($scope, dataService) {
 	var fixedDayModel = dataService.getDayRecordCollection().models[0];
 
+	behaviorTypeCollection = dataService.getBehaviorTypeCollection();
+	behaviorIncidentCollection = dataService.getBehaviorIncidents();
+	timeRecordCollection = dataService.getTimeRecordCollection();
+
 	// view model functions
 	function EntryBucket(timeRecord, behaviorTypes, behaviorIncidents) {
-		this.active = false;
-		this.time_string = timeRecord.get('iso_string');
+		this.timeRecordId = timeRecord.id;
+		this.isoTimeString = timeRecord.get('iso_string');
 		this.markers = {};
 
 		// create markers for each behavior type
@@ -40,19 +44,61 @@ app.controller('TrackerController', function($scope, dataService) {
 		this.tallyMode = behaviorType.get('tallyMode');
 		this.behaviorTypeId = behaviorType.id;
 		this.isDisabled = false;
-		this.onTap = function() {
-			console.log('tapped behavior ' + this.code);
-		};
+	}
+
+
+	function getIncidents(behaviorTypeId, timeRecordId) {
+		var behaviorTypeModel = behaviorTypeCollection.get(behaviorTypeId);
+		var timeRecordModel = timeRecordCollection.get(timeRecordId);
+		if (!behaviorTypeModel || !timeRecordModel) {
+			console.error('invalid ids. cannot get incidents')
+		}		
+		return behaviorIncidentCollection.where({time: timeRecordModel, day: fixedDayModel, behaviorType: behaviorTypeModel});
+	}
+
+	function addIncident(behaviorTypeId, timeRecordId) {
+		var behaviorTypeModel = behaviorTypeCollection.get(behaviorTypeId);
+		var timeRecordModel = timeRecordCollection.get(timeRecordId);
+		if (!behaviorTypeModel || !timeRecordModel) {
+			console.error('invalid ids. cannot add incident')
+		}
+		var newIncidentModel = dataService.addBehaviorIncident(
+			behaviorTypeModel, fixedDayModel, timeRecordModel);
+
+		var marker = $scope.activeBucket.markers[behaviorTypeId];
+		if (!marker) {
+			marker = new BucketMarker(bType);
+		}
+		marker.count++;
+	}
+
+	function removeIncident(behaviorTypeId, timeRecordId) {
+		var incidents = getIncidents(behaviorTypeId, timeRecordId);
+		if (incidents.length > 0) {
+			dataService.removeBehaviorIncident(incidents[0]);
+		}
+
+		var marker = $scope.activeBucket.markers[behaviorTypeId];
+		if (marker) {
+			marker.count--;
+		}
+	}
+
+	// this function is an artifact of bad design. get rid of after refactoring
+	function getEntryBucketForTimeId(timeRecordId) {
+		var matches = _.where($scope.entryBuckets, {timeRecordId: timeRecordId});
+		if (matches.length > 0) {
+			return matches[0];
+		}
+		else {
+			return null;
+		}
 	}
 
 	function init() {
 		$scope.currentDay = {
 			label: 'Today'
 		};
-
-		var behaviorTypeCollection = dataService.getBehaviorTypeCollection();
-		var behaviorIncidentCollection = dataService.getBehaviorIncidents();
-		var timeRecordCollection = dataService.getTimeRecordCollection();
 
 		$scope.behaviorButtons = _.map(behaviorTypeCollection.models, function(behaviorTypeModel) {
 			return new BehaviorButton(behaviorTypeModel);
@@ -64,11 +110,44 @@ app.controller('TrackerController', function($scope, dataService) {
 				behaviorIncidentCollection.where({time: timeRecordModel, day: fixedDayModel})
 			);
 		});
+
+		// determine which bucket is active at first
+		$scope.activeBucket = null;
+		var timeNow = '09:03';//moment().format('hh:mm');
+		for (var i = 0; i < $scope.entryBuckets.length; ++i) {
+			console.log(timeNow + ' >? ' + $scope.entryBuckets[i].isoTimeString);
+			if (timeNow < $scope.entryBuckets[i].isoTimeString) {
+				break;
+			} 
+			else {
+				$scope.activeBucket = $scope.entryBuckets[i];
+			}
+		}
+
+		$scope.setActiveBucket = function(entryBucket) {
+			$scope.activeBucket = entryBucket;
+		};
+
+		$scope.behaviorTapped = function(behaviorTypeId) {
+			if (!$scope.activeBucket) {
+				console.error('No displayed time bucket is active.');
+				return;
+			}
+
+			var behaviorTypeModel = behaviorTypeCollection.get(behaviorTypeId);
+			var timeRecordId = $scope.activeBucket.timeRecordId;
+			if (behaviorTypeModel.get('tallyMode') || getIncidents(behaviorTypeId, timeRecordId).length === 0) {
+				addIncident(behaviorTypeId, timeRecordId);
+			}
+			else {
+				removeIncident(behaviorTypeId, timeRecordId);
+			}
+		};
 	}
 
 	init();
 
-	_glob = $scope;
+	_glob = behaviorIncidentCollection;
 });
 
 		// /*
